c9ab1b5e07935e6ba645998e71f1ecee5bf36b73
Thomas Tavolara <ttavolar@u.rochester.edu>
2016-03-05 22:13:11 -0500

    finished?

--- setup.sh
+++ setup.sh
@@ -16,3 +16,19 @@ mkdir sparkfunadc
 git clone https://github.com/flowthings/sparkfunAdc.git
 install -g sparkfunadc/
 
+#setup FFMPEG
+echo "Please plug in camera"
+$LSUSB = lsusb
+if [ -n "$LSUSB" ]; then
+	echo "not connected..."
+	sleep 5000
+fi
+$DEV_VIDEO_s = "/dev/video0"
+$DEV_VIDEO_e = /dev/video0
+if [ -eq $DEV_VIDEO_s $DEV_VIDEO_e ]
+	
+fi
+git clone https://github.com/drejkim/edi-cam.git
+/home/root/edi-cam/bin/install_ffmpeg.sh
+/home/root/bin/ffmpeg/ffmpeg -f alsa -thread_queue_size 32768 -itsoffset -0.8 -ac 1 -t 15.8 -i hw:2 -f video4linux2 -thread_queue_size 8388608  -itsoffset 00:00 -s hd480  -t 15 -i /dev/video0 -b:v 1M -c:v libxvid -vsync cfr /home/root/Videos/test.avi
+
