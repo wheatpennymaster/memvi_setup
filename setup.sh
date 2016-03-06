ce13029ff053e727b00a517c4fb0c005d7bf00af
Thomas Tavolara <ttavolar@u.rochester.edu>
2016-03-06 11:03:40 -0500

    Doned

--- setup.sh
+++ setup.sh
@@ -12,9 +12,8 @@ mkdir YesFiles
 mkdir NoFiles
 mkdir Videos
 mkdir 9DOF_folder
-mkdir sparkfunadc
 git clone https://github.com/flowthings/sparkfunAdc.git
-install -g sparkfunadc/
+npm install -g sparkfunAdc/
 
 #setup FFMPEG
 echo "Please plug in camera"
@@ -25,13 +24,13 @@ do
 	echo "not connected..."
 	sleep 5
 done
-DEV_VIDEO_s="dev/video0"
-DEV_VIDEO_e=`/dev/video0`
-if [ "$DEV_VIDEO_s" != "$DEV_VIDEO_e" ]; then
-	echo "DEV0 error"
-	exit
-fi
+#fix this
+#DEV_VIDEO_s="/dev/video0"
+#DEV_VIDEO_e=`/dev/video0`
+#if [ "$DEV_VIDEO_s" != "$DEV_VIDEO_e" ]; then
+#	echo "DEV0 error"
+#	exit
+#fi
 git clone https://github.com/drejkim/edi-cam.git
 /home/root/edi-cam/bin/install_ffmpeg.sh
 /home/root/bin/ffmpeg/ffmpeg -f alsa -thread_queue_size 32768 -itsoffset -0.8 -ac 1 -t 15.8 -i hw:2 -f video4linux2 -thread_queue_size 8388608  -itsoffset 00:00 -s hd480  -t 15 -i /dev/video0 -b:v 1M -c:v libxvid -vsync cfr /home/root/Videos/test.avi
-
